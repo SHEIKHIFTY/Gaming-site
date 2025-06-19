91721cc2d7923a3ba5178fcfd4107035b5752b54
Sheikh Ifty <sheikhiftekhar003@gmail.com>
2025-06-19 11:37:07 +0600

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "#FF42A5",
+        "secondary-color": "#4EC5F7",
+      },
+    },
   },
   plugins: [],
 }
